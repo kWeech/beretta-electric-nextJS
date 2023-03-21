15eaee70bd4c4ead91680eca61c48de3971ad9da
Your Name <kenneth.weech@gmail.com>
2023-03-21 16:11:34 -0600

    hopefully fixed indexing

--- beretta-electric-app/pages/_document.js
+++ beretta-electric-app/pages/_document.js
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta
+        name="google-site-verification"
+        content="3cplThKgaVu_s4-ryw3d4MgsywIDYDS0u-WWibCCL1Q"
+      />
       <body>
         <Main />
         <div id="sideBar" />
